7d4161c18f0c050662280cbcb626ca20445336ba
Aral Tasher <araltasher@Arals-MacBook-Pro.local>
2017-09-25 03:03:42 -0400

    added loading animation

--- app/components/Tab.js
+++ app/components/Tab.js
@@ -97,7 +97,17 @@ class Tab extends React.Component {
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage}
                 />
-                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos} />}
+                {!this.state.repos ? <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1"></div>
+        <div className="sk-cube sk-cube2"></div>
+        <div className="sk-cube sk-cube3"></div>
+        <div className="sk-cube sk-cube4"></div>
+        <div className="sk-cube sk-cube5"></div>
+        <div className="sk-cube sk-cube6"></div>
+        <div className="sk-cube sk-cube7"></div>
+        <div className="sk-cube sk-cube8"></div>
+        <div className="sk-cube sk-cube9"></div>
+      </div> : <RepoGrid repos={this.state.repos} />}
             </div>
         )
     }
