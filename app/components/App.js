737218d07a107b74c03bb6ffd48b2217d062e5b9
araltasher <araltasher@gmail.com>
2017-09-25 16:07:28 -0400

    React Router added

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,35 @@
-var React = require('react');
-var Tab = require('./Tab');
+var React = require("react");
+var Tab = require("./Tab");
+var Nav = require("./Nav");
+var Home = require("./Home");
+var Battle = require("./Battle");
+var ReactRouter = require("react-router-dom");
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <Tab />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/battle" component={Battle} />
+            <Route path="/popular" component={Tab} />
+
+            <Route
+              render={function() {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 module.exports = App;
--- app/components/Tab.js
+++ app/components/Tab.js
@@ -1,116 +1,127 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var api = require('../utils/api');
+var React = require("react");
+var PropTypes = require("prop-types");
+var api = require("../utils/api");
 
 function SelectLanguage(props) {
-    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
-    return (
-        <ul className="languages">
-            {languages.map(function (lang) {
-                return (
-                    <li
-                        style={lang === props.selectedLanguage ? { color: '#6bb9f0' } : null}
-                        onClick={props.onSelect.bind(null, lang)}
-                        key={lang}>
-                        {lang}
-                    </li>
-                )
-            }, this)}
-        </ul>
-    )
+  var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+  return (
+    <ul className="languages">
+      {languages.map(function(lang) {
+        return (
+          <li
+            style={
+              lang === props.selectedLanguage ? { color: "#d0021b" } : null
+            }
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 function RepoGrid(props) {
-    return (
-        <ul className='popular-list'>
-            {props.repos.map(function (repo, index) {
-                return (
-                    <li key={repo.name} className='popular-item'>
-                        <div className='popular-rank'>#{index + 1}</div>
-                        <ul className='space-list-items'>
-                            <li>
-                                <a href={repo.html_url}>
-                                    <img
-                                        className='avatar'
-                                        src={repo.owner.avatar_url}
-                                        alt={'Avatar for ' + repo.owner.login}
-                                    /></a>
-                            </li>
-                            <li><a href={repo.html_url}>{repo.name}</a></li>
-                            <li>@{repo.owner.login}</li>
-                            <li>{repo.stargazers_count} starts</li>
-                        </ul>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+  return (
+    <ul className="popular-list">
+      {props.repos.map(function(repo, index) {
+        return (
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rank">#{index + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <a href={repo.html_url}>
+                  <img
+                    className="avatar"
+                    src={repo.owner.avatar_url}
+                    alt={"Avatar for " + repo.owner.login}
+                  />
+                </a>
+              </li>
+              <li>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} starts</li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 RepoGrid.propTypes = {
-    repos: PropTypes.array.isRequired,
-
-}
+  repos: PropTypes.array.isRequired
+};
 
 SelectLanguage.propTypes = {
-    selectedLanguage: PropTypes.string.isRequired,
-    onSelect: PropTypes.func.isRequired,
-}
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
 
 class Tab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All",
+      repos: null
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedLanguage: 'All',
-            repos: null
-        };
-        this.updateLanguage = this.updateLanguage.bind(this);
-    }
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
 
-    componentDidMount() {
-        this.updateLanguage(this.state.selectedLanguage);
-    }
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang,
+        repos: null
+      };
+    });
 
-    updateLanguage(lang) {
-        this.setState(function () {
-            return {
-                selectedLanguage: lang,
-                repos: null,
-            }
+    api.fetchPopularRepos(lang).then(
+      function(repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          };
         });
+      }.bind(this)
+    );
+  }
 
-        api.fetchPopularRepos(lang)
-            .then(function (repos) {
-                this.setState(function () {
-                    return {
-                        repos: repos
-                    }
-                })
-            }.bind(this));
-    }
-
-    render() {
-        return (
-            <div>
-                <SelectLanguage
-                    selectedLanguage={this.state.selectedLanguage}
-                    onSelect={this.updateLanguage}
-                />
-                {!this.state.repos ? <div className="sk-cube-grid">
-        <div className="sk-cube sk-cube1"></div>
-        <div className="sk-cube sk-cube2"></div>
-        <div className="sk-cube sk-cube3"></div>
-        <div className="sk-cube sk-cube4"></div>
-        <div className="sk-cube sk-cube5"></div>
-        <div className="sk-cube sk-cube6"></div>
-        <div className="sk-cube sk-cube7"></div>
-        <div className="sk-cube sk-cube8"></div>
-        <div className="sk-cube sk-cube9"></div>
-      </div> : <RepoGrid repos={this.state.repos} />}
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        {!this.state.repos ? (
+          <div className="home-container">
+            <div className="sk-cube-grid">
+              <div className="sk-cube sk-cube1" />
+              <div className="sk-cube sk-cube2" />
+              <div className="sk-cube sk-cube3" />
+              <div className="sk-cube sk-cube4" />
+              <div className="sk-cube sk-cube5" />
+              <div className="sk-cube sk-cube6" />
+              <div className="sk-cube sk-cube7" />
+              <div className="sk-cube sk-cube8" />
+              <div className="sk-cube sk-cube9" />
             </div>
-        )
-    }
+          </div>
+        ) : (
+          <RepoGrid repos={this.state.repos} />
+        )}
+      </div>
+    );
+  }
 }
 
 module.exports = Tab;
